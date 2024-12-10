bd6c3652d3b07a31a4c3a155f2b1beedd2004bb2
archibald-jpg <archie.whiting0707@gmail.com>
2024-12-10 15:31:09 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
   createCanvas(400, 400);
+  mic = new p5.AudioIn();
+  mic.start();
+  
 }
 
 function draw() {
-  background(220);
+  let vol = mic.getLevel();
+  
+  background(0);
+  noStroke();
+  fill('white');
+  circle(200, 200, 200 + 200 * noise(frameCount / 0.5))
+  fill('red')
+  stroke('red');
+  circle(200, 200, 180 + 20 * noise(frameCount / 0.1))
+  noFill();
+  stroke('blue');
 }
